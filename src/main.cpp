4eead9c9e17a34d05305bc529f602c0b38128ad9
K3r1m <70035028+K3r1m@users.noreply.github.com>
2022-10-25 10:05:22 +0200

    Nearly destroyed my laptop, file structure works

---
+++ src/main.cpp
@@ -0,0 +1,115 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <IBusBM.h>
+#include <pinout.h> //in this File, all the used Pins and used Variables are declared to make the code more clear.
+
+
+IBusBM ibusRc;
+
+HardwareSerial& ibusRcSerial = Serial2;
+HardwareSerial& debugSerial = Serial;
+
+void setup() {
+    pinMode(csPin, OUTPUT);
+    //pinMode(csPin2, OUTPUT);
+    pinMode(direction, OUTPUT);
+    pinMode(linearmotor_back_open, OUTPUT);
+    pinMode(linearmotor_back_close, OUTPUT);
+    ibusRc.begin(ibusRcSerial);
+    Serial.begin(9600);
+    SPI.begin();
+}
+// Read the number of a given channel and convert to the range provided.
+// If the channel is off, return the default value
+int readChannel(byte channelInput, int minLimit, int maxLimit, int defaultValue){
+  uint16_t ch = ibusRc.readChannel(channelInput);
+  if (ch < 100) return defaultValue;
+  
+  return map(ch, 1000, 2000, minLimit, maxLimit);
+}
+
+void turnleft_vorne() {
+  digitalWrite(linearmotor_back_open, HIGH); //Weil der Motor an der Linken Seite befestigt wird.
+  digitalWrite(linearmotor_back_close, LOW); 
+  Serial.println("turning left.");
+}
+void turnright_vorne(){
+  digitalWrite(linearmotor_back_open, LOW); //Weil der Motor an der Linken Seite befestigt wird.
+  digitalWrite(linearmotor_back_close, HIGH);
+  Serial.println("turning right hinten.");
+}
+void block_vorne(){
+  digitalWrite(linearmotor_back_open, LOW); //Weil der Motor an der Linken Seite befestigt wird.
+  digitalWrite(linearmotor_back_close, LOW);
+  Serial.println("linearmotor standby");
+}
+void set_steering_front() {
+  if (arrayChannel[0] > 10) {
+    turnright_vorne();
+  }
+  else if (arrayChannel[0] <-10) {
+    turnleft_vorne();
+  }
+  else{
+    block_vorne();
+  }
+}
+
+void get_tgy_data()
+{
+  for (byte i = 0; i<10; i++){ //Alle Channels auslesen.
+    //int value = readChannel(i, -100, 100, 0);
+    arrayChannel[i] = readChannel(i, -100, 100, 0);
+    debugSerial.print("Ch");
+    debugSerial.print(i + 1);
+    debugSerial.print(": ");
+    debugSerial.print(arrayChannel[i]);
+    //debugSerial.print(value);
+    debugSerial.print(" "); 
+}
+}
+
+void set_motor_speed()
+{
+   //allMotors
+  current_motor_speed = map(arrayChannel[2],100,-80,0,128); //Auslesen der Daten an Channel 3.
+  digitalWrite(csPin, LOW);
+  
+  //SPI.transfer(0x00);
+  SPI.transfer(0);
+  SPI.transfer(current_motor_speed);
+  //delay(10);
+  digitalWrite(csPin, HIGH); 
+    
+  Serial.println(current_motor_speed);
+}
+
+void set_brakes(){
+  if (arrayChannel[8]==100){ //Bremsen und Richtungswechsel (it's not a bug it's a feature.) funktioniert mit allen Motorcontrollern
+    digitalWrite(direction, HIGH);
+    delay(1010); //angenährte Zeit, welche der Controller braucht um die Richtung zu wechseln. Dabei sollte so wenig Zeit benötigt werden, damit der Controller nicht eine Geschwindigkeit von 50% erreicht.
+    digitalWrite(direction, LOW);
+    Serial.println("on");
+    }
+    else {
+    digitalWrite(direction, LOW);
+    Serial.println("off");
+    } 
+}
+
+void set_light(){}
+void loop() {
+
+    
+  get_tgy_data();
+  
+  set_motor_speed();
+  
+  set_steering_front();
+
+  set_brakes();
+  
+   
+
+}
+
---
+++ src/pinout.h
@@ -0,0 +1,20 @@
+#ifndef PINOUT_H
+#define PINOUT_H
+#include <Arduino.h>
+
+//all Pins
+int csPin = 23;
+//int csPin2 = 23;
+int arrayChannel[10];
+int direction_motor = 27;
+int linearmotor_back_open = 34;
+int linearmotor_back_close = 35;
+int schweinwerfer_vorne = 3;
+
+//all Variables
+int current_motor_speed;
+int current_motor_speed2; 
+
+//Battery Compartiment
+
+#endif
